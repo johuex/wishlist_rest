b16e03391026d2e2e0f9ffef5430b22ac634b1e2
Dmitry Polyakov <polds@list.ru>
2020-12-25 13:20:09 +0300

    PROD RABOTAET!

--- connectDB.py
+++ connectDB.py
@@ -13,11 +13,6 @@ port = url.port
 
 def get_connection():
     conn = psycopg2.connect(
-                            #dbname="wlist",
-                            #user="dimas",
-                            #password="123456",
-                            #host="db-course.cvjxeoubdpa4.eu-central-1.rds.amazonaws.com",
-                            #cursor_factory=DictCursor
                             dbname=dbname,
                             user=user,
                             password=password,
