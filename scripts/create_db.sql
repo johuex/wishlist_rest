4a13d3b23050782e5c19b336027820a7961940c0
Dmitry Polyakov <polds@list.ru>
2021-03-01 17:47:43 +0300

    update v2 DB schema; update create_db.sql; moving DB to local

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,204 +1,186 @@
-create table "users"
+create table users
 (
-	"user_id" integer not null
-		constraint user_pk
+	id serial
+		constraint users_pk
 			primary key,
-	phone_number varchar(12) not null,
+	phone_number varchar(20),
 	user_name varchar(50) not null,
-	surname varchar(50) not null,
-	userpic varchar(255),
-	about varchar(255),
-	birthday date not null,
-    password_hash varchar(255),
-    nickname varchar(50) not null,
-    email varchar(255) not null,
-    last_seen timestamp
+	surname varchar(50),
+	userpic text,
+	about varchar(1000),
+	birthday date,
+	password_hash text not null,
+	nickname varchar(100) not null,
+	email varchar(255) not null,
+	token text,
+	token_expiration timestamptz,
+	last_seen timestamptz
 );
-create unique index users_nickname_uindex
-	on users (nickname);
 
 create unique index users_email_uindex
 	on users (email);
 
-create sequence "users_user_id_seq";
-
-alter table users alter column "user_id" set default nextval('public."users_user_id_seq"');
-
-alter sequence "users_user_id_seq" owned by users."user_id";
-
-alter table users owner to dimas;
-
-create table friendship
-(
-	"user_id_1" int not null,
-	"user_id_2" int not null,
-	constraint friendship_pk
-		primary key ("user_id_1", "user_id_2"),
-	constraint friendship_user_user_id_user_id_fk_1
-		foreign key ("user_id_1") references users ("user_id")
-			on update cascade on delete cascade,
-	constraint friendship_user_user_id_user_id_fk_2
-		foreign key ("user_id_2") references users ("user_id")
-			on update cascade on delete cascade
-);
-
-create table friends_requests
-(
-	user_id_from int not null
-		constraint friends_requests_users_user_id_fk
-			references users
-				on update cascade on delete cascade,
-	user_id_to int not null
-		constraint friends_requests_users_user_id_fk_2
-			references users
-				on update cascade on delete cascade,
-	constraint friends_requests_pk
-		primary key (user_id_from, user_id_to)
-);
-
-
+create unique index users_nickname_uindex
+	on users (nickname);
 
 create table wishlist
 (
-	list_id int not null
+	id serial
 		constraint wishlist_pk
 			primary key,
-	"user_id" int not null
-		constraint wishlist_user_user_id_fk
+	user_id int not null
+		constraint wishlist_users_id_fk
 			references users
 				on update cascade on delete cascade,
 	title varchar(50) not null,
 	about varchar(255),
-	access_level bool not null
+	access_level int not null
 );
 
-create sequence wishlist_list_id_seq;
-
-alter table wishlist alter column list_id set default nextval('public.wishlist_list_id_seq');
-
-alter sequence wishlist_list_id_seq owned by wishlist.list_id;
-
-create table group_wishlist
+create table "group"
 (
-	"group_id" int not null
-		constraint group_wishlist_pk
+	id serial
+		constraint group_pk
 			primary key,
-	title varchar(255) not null,
-	about varchar(255),
-	date date not null
-);
-
-create table group_user
-(
-	"group_id" int not null
-		constraint group_user_group_wishlist_group_id_fk
-			references group_wishlist
-				on update cascade on delete cascade,
-	"user_id" int not null
-		constraint group_user_user_user_id_fk
-			references users
-				on update cascade on delete cascade,
-	role int not null,
-	constraint group_user_pk
-		primary key ("group_ID", "user_ID")
+	title varchar(50) not null,
+	about varchar(1000),
+	date timestamptz not null
 );
 
 create table item
 (
-	"item_id" int not null
+	id serial
 		constraint item_pk
 			primary key,
 	title varchar(50) not null,
-	about varchar(255) not null,
-	access_level bool not null,
-	picture varchar(255),
-	"giver_id" int
-		constraint item_user_user_id_fk
+	about varchar(255),
+	access_level int not null,
+	status int not null,
+	giver_id int
+		constraint item_users_id_fk
 			references users
-				on update cascade on delete cascade
+				on update cascade on delete cascade,
+	date_creation date not null,
+	date_for_status date
+);
+
+create table friend_requests
+(
+	user_id_from int
+		constraint friend_requests_users_id_fk
+			references users
+				on update cascade on delete cascade,
+	user_id_to int
+		constraint friend_requests_users_id_fk_2
+			references users
+				on update cascade on delete cascade,
+	constraint friend_requests_pk
+		primary key (user_id_from, user_id_to)
+);
+
+create table friendship
+(
+	user_id_1 int
+		constraint friendship_users_id_fk
+			references users
+				on update cascade on delete cascade,
+	user_id_2 int
+		constraint friendship_users_id_fk_2
+			references users
+				on update cascade on delete cascade,
+	constraint friendship_pk
+		primary key (user_id_1, user_id_2)
+);
+
+create table user_item
+(
+	user_id int
+		constraint user_item_users_id_fk
+			references users
+				on update cascade on delete cascade,
+	item_id int
+		constraint user_item_item_id_fk
+			references item
+				on update cascade on delete cascade,
+	constraint user_item_pk
+		primary key (user_id, item_id)
 );
 
 create table item_list
 (
-	"list_id" int not null
-		constraint item_list_wishlist_list_id_fk
+	list_id int
+		constraint item_list_wishlist_id_fk
 			references wishlist
 				on update cascade on delete cascade,
-	"item_id" int not null
-		constraint item_list_item_item_id_fk
+	item_id int
+		constraint item_list_item_id_fk
 			references item
 				on update cascade on delete cascade,
 	constraint item_list_pk
-		primary key ("list_id", "item_id")
+		primary key (list_id, item_id)
 );
 
-create table status
+create table group_list
 (
-	status varchar(50) not null
-		constraint table_name_pk
-			primary key
+	group_id int
+		constraint group_list_group_id_fk
+			references "group"
+				on update cascade on delete cascade,
+	list_id int
+		constraint group_list_wishlist_id_fk
+			references wishlist
+				on update cascade on delete cascade,
+	constraint group_list_pk
+		primary key (group_id, list_id)
+);
+
+create table group_user
+(
+	group_id int
+		constraint group_user_group_id_fk
+			references "group"
+				on update cascade on delete cascade,
+	user_id int
+		constraint group_user_users_id_fk
+			references users
+				on update cascade on delete cascade,
+	role_in_group int,
+	constraint group_user_pk
+		primary key (group_id, user_id)
 );
 
 create table degree_of_desire
 (
-    degree_id serial not null
-        constraint degree_of_desire_pk
-			primary key,
+	item_id int
+		constraint degree_of_desire_pk
+			primary key
+		constraint degree_of_desire_item_id_fk
+			references item
+				on update cascade on delete cascade,
 	degree varchar(50) not null
-
 );
 
-create table item_status
+create table item_picture
 (
-	"item_id" int not null
-		constraint item_status_item_item_id_fk
+	item_id int
+		constraint item_picture_pk
+			primary key
+		constraint item_picture_item_id_fk
 			references item
 				on update cascade on delete cascade,
-	status varchar(50) not null
-		constraint item_status_status_status_fk
-			references status
-				on update cascade on delete cascade,
-	constraint item_status_pk
-		primary key ("item_id", status)
+	path_to_picture int not null
 );
 
-create table item_degree
+create table item_group
 (
-	"item_id" int not null
-		constraint item_degree_item_item_id_fk
+	group_id int
+		constraint item_group_group_id_fk
+			references "group"
+				on update cascade on delete cascade,
+	item_id int
+		constraint item_group_item_id_fk
 			references item
 				on update cascade on delete cascade,
-	degree_id int not null
-		constraint item_degree_degree_of_desire_degree_id_fk
-			references degree_of_desire
-				on update cascade on delete cascade,
-	constraint item_degree_id_pk
-		primary key ("item_id", degree_id)
+	constraint item_group_pk
+		primary key (item_id, group_id)
 );
-alter table item_degree
-	add constraint item_degree_degree_of_desire_degree_id_fk
-		foreign key (item_id) references degree_of_desire
-			on update cascade on delete cascade;
-
-alter table item_degree
-	add constraint item_degree_item_item_id_fk
-		foreign key (item_id) references item
-			on update cascade on delete cascade;
-
-create table user_item
-(
-	user_id int not null
-		constraint user_item_users_user_id_fk
-			references users
-				on update cascade on delete cascade,
-	item_id int not null
-		constraint user_item_item_item_id_fk
-			references item
-				on update cascade on delete cascade,
-	constraint user_item_pk
-		primary key (item_id, user_id)
-);
-
-
-
-
